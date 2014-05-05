900f5cd758ff3cea9d1b96ea1bec41e618159471
c_14 <c-14git.shadowmurderer@spamgourmet.com>
2014-05-05 16:58:56 +0200

    Implement markov chain text generation and update LHashMap to use Numeric.Probability.

---
+++ src/MBE/Markov.hs
@@ -0,0 +1,45 @@
+module MBE.Markov
+    ( recChain
+    ) where
+
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as C
+import qualified Data.HashMap.Strict as H
+import qualified Numeric.Probability.Random as R
+import System.Random
+
+import MDB.LHashMap
+
+space = C.singleton ' '
+eol = C.singleton '$'
+sol = C.singleton '^'
+
+punctuation :: [B.ByteString]
+punctuation = map C.singleton [',','.',';','\t','!','?','$']
+
+notPunctuation :: B.ByteString -> Bool
+notPunctuation = flip notElem punctuation
+
+
+recChain :: FilePath -> StdGen -> IO B.ByteString
+recChain path g = do
+        db <- importDB path
+        let wList = listFunc g db []
+        let rList = reverse wList
+        return (foldl B.append B.empty rList)
+
+listFunc :: StdGen -> PDatabase -> [B.ByteString] -> [B.ByteString]
+listFunc g db l = if null l then listFunc g2 db fstList else eolChck
+        where
+            eolChck = if head l == eol then tail l else f
+            (g1,g2) = split g
+            newList = if notPunctuation nxtWord then (nxtWord : space : l) else (nxtWord : l)
+            nxtWord = getNextWord g1 db cw
+            fstList = ((getNextWord g1 db sol) : l)
+            cw      = if null l then sol else head l
+            f       = listFunc g2 db newList
+
+getNextWord :: StdGen -> PDatabase -> B.ByteString -> B.ByteString
+getNextWord g db cw = R.runSeed g $ R.pick cwDist
+    where
+        cwDist  = db H.! cw
--- src/MDB/LHashMap.hs
+++ src/MDB/LHashMap.hs
@@ -6,29 +6,36 @@ module MDB.LHashMap
 
 import qualified Data.ByteString.Char8 as C
 import qualified Data.HashMap.Strict as H
+import Numeric.Probability.Distribution
 import System.IO
 
 import MDB.PHashMap
 
 type Probability = Float
-type WPPHMap = H.HashMap Word Probability
+type WPSHMap = H.HashMap Word Probability
+type WPPHMap = T Probability Word
+type SDatabase = H.HashMap Word WPSHMap
 type PDatabase = H.HashMap Word WPPHMap
 
 importDB :: FilePath -> IO PDatabase
 importDB path = do
         db <- checkDB path
-        return (calcProbability db)
+        let bs = calcProbability db
+        return (probConv bs)
 
 pWriteDB :: FilePath -> PDatabase -> IO ()
 pWriteDB path db = withFile path WriteMode (\handle ->
                                         C.hPutStr handle . C.pack . show $ H.toList wpm)
                                         where
-                                            wpm = H.map H.toList db
+                                            wpm = db
 
-calcProbability :: PDatabase -> PDatabase
+calcProbability :: SDatabase -> SDatabase
 calcProbability = H.map probGet
 
-probGet :: WPPHMap -> WPPHMap
+probGet :: WPSHMap -> WPSHMap
 probGet wp = H.map (/ sm) wp
     where
         sm = H.foldl' (+) 0 wp
+
+probConv :: SDatabase -> PDatabase
+probConv = H.map (fromFreqs . H.toList)
